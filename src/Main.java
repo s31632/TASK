4d2736d84e6d38241460541faac5f845c7f6950f
s31632 <s31632@pjwstk.edu.pl>
2025-03-14 15:09:53 +0100

    TASK1

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class int {} filterEvenIndexOddValue(int[] array);
+int [] Array = {1, 2, 3, 4, 5, 6 };
+int [] Array1 = {2, 3, 4, 5, 6, 7};
+int [] Array2 = {7, 8, 9, 10, 11};
+for (int
